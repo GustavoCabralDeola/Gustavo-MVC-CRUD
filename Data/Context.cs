c022b29e5d42eacd679defab48efffbfd7ced802
Gustavo Deola <gustavodeola@gmail.com>
2023-05-16 20:37:58 -0300

    Adicionar arquivos de projeto.

---
+++ Data/Context.cs
@@ -0,0 +1,15 @@
+﻿using Gustavo_MVC_CRUD.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Gustavo_MVC_CRUD.Data {
+    public class Context : DbContext {
+
+        public Context(DbContextOptions<Context> options) : base(options) 
+        {
+
+        }
+
+        public DbSet<User> Usuarios { get; set; }
+
+    }
+}
---
+++ Models/User.cs
@@ -0,0 +1,31 @@
+﻿namespace Gustavo_MVC_CRUD.Models {
+    public class User {
+      
+            public User() { }
+
+            public User
+            (
+              int id,
+              string name,
+              string company,
+              string email,
+              string personalphone,
+              string commercialphone
+            ) {
+                Id = id;
+                Name = name;
+                Company = company;
+                Email = email;
+                PersonalPhone = personalphone;
+                CommercialPhone = commercialphone;
+            }
+
+            public int Id { get; set; }
+            public string Name { get; set; }
+            public string Company { get; set; }
+            public string Email { get; set; }
+            public string PersonalPhone { get; set; }
+            public string CommercialPhone { get; set; }
+
+        }
+    }
