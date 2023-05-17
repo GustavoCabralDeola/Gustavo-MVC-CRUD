bd1fcf49638d1d1d312b0046224a5119e502e9d2
Gustavo Deola <gustavodeola@gmail.com>
2023-05-16 23:08:47 -0300

    feat: Controller, Models

--- Models/User.cs
+++ Models/User.cs
@@ -1,4 +1,9 @@
-﻿namespace Gustavo_MVC_CRUD.Models {
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Gustavo_MVC_CRUD.Models {
+
+    [Table("User")]
     public class User {
       
             public User() { }
@@ -20,12 +25,29 @@
                 CommercialPhone = commercialphone;
             }
 
+            [Display(Name = "Id")]
             public int Id { get; set; }
-            public string Name { get; set; }
-            public string Company { get; set; }
-            public string Email { get; set; }
-            public string PersonalPhone { get; set; }
-            public string CommercialPhone { get; set; }
 
+            [Display(Name = "Name")]
+            [Required(ErrorMessage = "{0} required")]
+            [StringLength(60, MinimumLength = 3, ErrorMessage = "{0} size should be between {2} and {1}")]
+            public string Name { get; set; }
+
+            [Display(Name = "Company")]
+            public string Company { get; set; }
+
+            
+            [EmailAddress(ErrorMessage = "Enter a valid email")]
+            [DataType(DataType.EmailAddress)]
+            [Display(Name = "Email")]
+            public string Email { get; set; }
+            
+            [Display(Name = "Personal_Phone")]
+            [StringLength(15,MinimumLength = 11, ErrorMessage = "Enter a valid number")]
+            public string PersonalPhone { get; set; }
+
+            [Display(Name = "Comercial_Phone")]
+            [StringLength(15, MinimumLength = 11, ErrorMessage = "Enter a valid number")]
+            public string CommercialPhone { get; set; }
         }
     }
